9e9bfc21dfc2ed5a5a7b467f986a41d91d15877d
Willis O'Leary <wolearyc@gmail.com>
2023-05-12 11:31:42 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='ramannoodle',
+      version='1.0',
+      description="Calculated Raman spectra from MD",
+      long_description="",
+      author="Willis O'Leary",
+      author_email='wolearyc@mit.edu',
+      license='TODO',
+      packages=['ramannoodle'],
+      zip_safe=False,
+      install_requires=[
+          'numpy',
+          'scipy', 
+          'matplotlib',
+          'spg',
+          'ase',
+          'phonopy',
+          'spglib'
+          ],
+      )
